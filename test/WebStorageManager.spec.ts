705fd31253b64c5abd150f8343db7dd98f840679
JdeJ <jorgedjuana@gmail.com>
2020-05-21 23:29:25 +0200

    Add basic testing config

--- test/WebStorageManager.spec.ts
+++
@@ -1,5 +0,0 @@
-import WebStorageManager from "../src";
-
-test("WebStorageManager is exported", () => {
-    expect(WebStorageManager).toBeInstanceOf(Object);
-});
--- test/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import WebStorageManager from '../src';
-
-describe("WebStorageManager is exported", () => {
-    it("sayHello() returns 'Hello Jorge'", () => {
-        expect(WebStorageManager.sayHello('Hello', 'Jorge')).toEqual('Hello Jorge');
-    });
-});
